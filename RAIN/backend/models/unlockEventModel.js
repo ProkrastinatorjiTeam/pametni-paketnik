2fcc056af885dcef90a6dcad8255cb0280296b5d
fl0rijan <florijan2001@gmail.com>
2025-05-31 14:11:15 +0200

    PP-73 Added success if successfully opened

--- RAIN/backend/models/unlockEventModel.js
+++ RAIN/backend/models/unlockEventModel.js
@@ -13,7 +13,8 @@ var unlockEventSchema = new Schema({
         required: true
     },
     'timestamp' : {type: Date, default: Date.now},
-    'authorized': {type: Boolean, default: false}
+    'authorized': {type: Boolean, default: false},
+    'success' : {type: Boolean, default: false}
 });
 
 module.exports = mongoose.model('unlockEvent', unlockEventSchema);
