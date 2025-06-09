aba565b32b1a6575548850d3e5bc93c9b0c3917c
Furman56 <furman.urban@gmail.com>
2025-06-09 18:00:10 +0200

    Popravljen app.test, da isce text ki obstaja v fontendu

--- RAIN/frontend/src/App.test.js
+++ RAIN/frontend/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders PrintHub link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const linkElements = screen.getAllByText(/printhub/i);
+  expect(linkElements.length).toBeGreaterThan(0);
 });
